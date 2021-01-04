7f9e7b8f4cd5e0ecae57756de98de10eed66f2e8
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2021-01-04 00:49:02 -0500

    Showing test data on HomePage

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,49 @@
 // can read more about Pages in the Next.js docs at:
 // https://nextjs.org/docs/basic-features/pages
 
+import { getPosts } from '@lib/firebase';
 import styles from '@styles/index.module.scss';
 
-const HomePage = () => (
+const getFormattedDate = (milliseconds) => {
+  const formatOptions = {
+    weekday: 'long',
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric',
+    timeZone: 'UTC',
+  };
+  const date = new Date(milliseconds);
+  return date.toLocaleDateString(undefined, formatOptions);
+};
+
+const HomePage = ({ posts }) => (
   <div className={styles.HomePage}>
-    <h1>Hello from HomePage!</h1>
+    <h1>Blog Posts</h1>
+    {posts.map((post) => (
+      <article key={post.slug}>
+        <img src={post.coverImage} alt={post.coverImageAlt} />
+        <div>
+          <h2>{post.title}</h2>
+          <span>{getFormattedDate(post.dateCreated)}</span>
+          <p
+            dangerouslySetInnerHTML={{
+              __html: `${post.content.substring(0, 200)}...`,
+            }}
+          ></p>
+        </div>
+      </article>
+    ))}
   </div>
 );
+
+export async function getServerSideProps() {
+  const posts = await getPosts();
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
 
 export default HomePage;
