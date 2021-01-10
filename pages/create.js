724cb3915aeb6c76b892e15e5bf70be38bced71c
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2021-01-10 15:05:49 -0500

    Adding form and controls to CreatePage

--- pages/create.js
+++ pages/create.js
@@ -1,9 +1,94 @@
+import { useState } from 'react';
 import styles from '@styles/create.module.scss';
 
-const CreatePage = () => (
-  <div styles={styles.CreatePage}>
-    <h1>Hello, from CreatePage!</h1>
-  </div>
-);
+const CreatePage = () => {
+  const [formValues, setFormValues] = useState({
+    title: '',
+    slug: '',
+    coverImage: '',
+    coverImageAlt: '',
+    content: '',
+  });
+
+  /*
+  This is the function we're passing to each control so we can capture
+  the value in it and store it in our `formValues` variable.
+  */
+  const handleChange = (e) => {
+    const id = e.target.id;
+    const newValue = e.target.value;
+
+    setFormValues({ ...formValues, [id]: newValue });
+  };
+
+  /*
+  This function is passed to the <form> and specifies what happens when
+  the form is submitted. For now, we're going to log our `formValues`
+  to verify that they are being managed correctly.
+  
+  Side note: we do not need to set an `onClick` for the <button> at the
+  end of the form because it has type="submit". This allows us to click
+  to submit the form or press the Enter key to submit it.
+  */
+  const handleSubmit = (e) => {
+    // This prevents the default functionality of submitting a form
+    e.preventDefault();
+
+    console.log(formValues);
+  };
+
+  return (
+    <div className={styles.CreatePage}>
+      <form onSubmit={handleSubmit}>
+        <h1>Create a new post</h1>
+        <div>
+          <label htmlFor="title">Title</label>
+          <input
+            id="title"
+            type="text"
+            value={formValues.title}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="slug">Slug</label>
+          <input
+            id="slug"
+            type="text"
+            value={formValues.slug}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="coverImage">Cover Image URL</label>
+          <input
+            id="coverImage"
+            type="text"
+            value={formValues.coverImage}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="coverImageAlt">Cover Image Alt</label>
+          <input
+            id="coverImageAlt"
+            type="text"
+            value={formValues.coverImageAlt}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="content">Content</label>
+          <textarea
+            id="content"
+            value={formValues.content}
+            onChange={handleChange}
+          />
+        </div>
+        <button type="submit">Create</button>
+      </form>
+    </div>
+  );
+};
 
 export default CreatePage;
