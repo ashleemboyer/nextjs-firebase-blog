d9625050bd41a7cdda2ffb0ba5062d6b5914ed49
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2021-01-10 14:19:26 -0500

    Adding basic CreatePage component

---
+++ pages/create.js
@@ -0,0 +1,9 @@
+import styles from '@styles/create.module.scss';
+
+const CreatePage = () => (
+  <div styles={styles.CreatePage}>
+    <h1>Hello, from CreatePage!</h1>
+  </div>
+);
+
+export default CreatePage;
