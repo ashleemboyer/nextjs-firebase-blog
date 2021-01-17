700cbbf3f0750e66775aa04b481358f65204aee8
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2021-01-17 15:01:57 -0500

    Adding new Layout component

---
+++ components/Layout/Layout.jsx
@@ -0,0 +1,3 @@
+const Layout = ({ children }) => <div>{children}</div>;
+
+export default Layout;
--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,14 +1,17 @@
 import { getPostBySlug } from '@lib/firebase';
 import { getFormattedDate } from '@lib/utils';
+import { Layout } from '@components';
 import styles from '@styles/post.module.scss';
 
 const PostPage = ({ post }) => (
-  <div className={styles.PostPage}>
-    <img src={post.coverImage} alt={post.coverImageAlt} />
-    <h1>{post.title}</h1>
-    <span>Published {getFormattedDate(post.dateCreated)}</span>
-    <p dangerouslySetInnerHTML={{ __html: post.content }}></p>
-  </div>
+  <Layout>
+    <div className={styles.PostPage}>
+      <img src={post.coverImage} alt={post.coverImageAlt} />
+      <h1>{post.title}</h1>
+      <span>Published {getFormattedDate(post.dateCreated)}</span>
+      <p dangerouslySetInnerHTML={{ __html: post.content }}></p>
+    </div>
+  </Layout>
 );
 
 export async function getServerSideProps(context) {
