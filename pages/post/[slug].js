241373a2cbf308d62f20907344fbc0941aab6033
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2021-01-17 15:31:59 -0500

    Redirecting to 404 if post doesn't exist

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,18 +1,32 @@
+import { useRouter } from 'next/router';
 import { getPostBySlug } from '@lib/firebase';
 import { getFormattedDate } from '@lib/utils';
 import { Layout } from '@components';
 import styles from '@styles/post.module.scss';
 
-const PostPage = ({ post }) => (
-  <Layout>
-    <div className={styles.PostPage}>
-      <img src={post.coverImage} alt={post.coverImageAlt} />
-      <h1>{post.title}</h1>
-      <span>Published {getFormattedDate(post.dateCreated)}</span>
-      <p dangerouslySetInnerHTML={{ __html: post.content }}></p>
-    </div>
-  </Layout>
-);
+const PostPage = ({ post }) => {
+  const router = useRouter();
+
+  if (!post && typeof window !== 'undefined') {
+    router.push('/404');
+    return;
+  }
+
+  if (!post) {
+    return null;
+  }
+
+  return (
+    <Layout>
+      <div className={styles.PostPage}>
+        <img src={post.coverImage} alt={post.coverImageAlt} />
+        <h1>{post.title}</h1>
+        <span>Published {getFormattedDate(post.dateCreated)}</span>
+        <p dangerouslySetInnerHTML={{ __html: post.content }}></p>
+      </div>
+    </Layout>
+  );
+};
 
 export async function getServerSideProps(context) {
   const post = await getPostBySlug(context.query.slug);
