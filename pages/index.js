7669a02688c7fc42e27b6d0ac70ed4de3847e9ea
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2021-01-17 14:10:37 -0500

    Adding links to each post

--- pages/index.js
+++ pages/index.js
@@ -31,6 +31,7 @@ const HomePage = ({ posts }) => (
               __html: `${post.content.substring(0, 200)}...`,
             }}
           ></p>
+          <a href={`/post/${post.slug}`}>Continue Reading</a>
         </div>
       </article>
     ))}
