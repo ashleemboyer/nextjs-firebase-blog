9fa79ad1da45b39db30319b5f48d517bf0a3b542
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2021-01-17 15:08:38 -0500

    Adding nav and link to home page to Layout

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -1,3 +1,14 @@
-const Layout = ({ children }) => <div>{children}</div>;
+import styles from './Layout.module.scss';
+
+const Layout = ({ children }) => (
+  <div className={styles.Layout}>
+    <nav>
+      <span>
+        <a href="/">My Next.js Blog</a>
+      </span>
+    </nav>
+    <main>{children}</main>
+  </div>
+);
 
 export default Layout;
