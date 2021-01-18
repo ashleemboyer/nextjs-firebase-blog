41008907627b5778914f037b5354b087510890fc
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2021-01-17 19:34:38 -0500

    Adding EditPage component

---
+++ pages/edit/[slug].js
@@ -0,0 +1,116 @@
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+import { getPostBySlug, updatePost } from '@lib/firebase';
+import { useAuth } from '@contexts/auth';
+import { Layout } from '@components';
+import styles from '@styles/edit.module.scss';
+
+const EditPage = ({ post }) => {
+  const router = useRouter();
+  const [user, userLoading] = useAuth();
+  const [values, setValues] = useState(post);
+  const [isLoading, setIsLoading] = useState(false);
+
+  if (userLoading) {
+    return null;
+  }
+
+  if (!user && typeof window !== 'undefined') {
+    router.push('/signin');
+    return null;
+  }
+
+  const handleChange = (e) => {
+    const id = e.target.id;
+    const newValue = e.target.value;
+
+    setValues({ ...values, [id]: newValue });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let missingValues = [];
+    Object.entries(values).forEach(([key, value]) => {
+      if (!value) {
+        missingValues.push(key);
+      }
+    });
+
+    if (missingValues.length > 1) {
+      alert(`You're missing these fields: ${missingValues.join(', ')}`);
+      return;
+    }
+
+    setIsLoading(true);
+    updatePost(values)
+      .then(() => {
+        setIsLoading(false);
+        router.push(`/post/${post.slug}`);
+      })
+      .catch((err) => {
+        alert(err);
+        setIsLoading(false);
+      });
+  };
+
+  return (
+    <Layout>
+      <div className={styles.EditPage}>
+        <form onSubmit={handleSubmit}>
+          <h1>Edit Post: {post.slug}</h1>
+          <div>
+            <label htmlFor="title">Title</label>
+            <input
+              id="title"
+              type="text"
+              value={values.title}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="coverImage">Cover Image URL</label>
+            <input
+              id="coverImage"
+              type="text"
+              value={values.coverImage}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="coverImageAlt">Cover Image Alt</label>
+            <input
+              id="coverImageAlt"
+              type="text"
+              value={values.coverImageAlt}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="content">Content</label>
+            <textarea
+              id="content"
+              value={values.content}
+              onChange={handleChange}
+            />
+          </div>
+          <button type="submit" disabled={isLoading}>
+            {isLoading ? 'Updating...' : 'Update'}
+          </button>
+        </form>
+      </div>
+    </Layout>
+  );
+};
+
+export async function getServerSideProps(context) {
+  const post = await getPostBySlug(context.query.slug);
+
+  return {
+    props: {
+      post,
+    },
+  };
+}
+
+export default EditPage;
